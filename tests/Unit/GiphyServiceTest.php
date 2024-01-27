99c972bcaae528a3cceb70184178652a53a10a29
Tehuel <torresbaldi.tehuel@gmail.com>
2024-01-27 19:35:52 -0300

    service test

--- tests/Unit/GiphyServiceTest.php
+++ tests/Unit/GiphyServiceTest.php
@@ -12,11 +12,29 @@ class GiphyServiceTest extends TestCase
     {
         $mockData = [1,2,3];
         Http::fake([
-            'api.giphy.com/*' => Http::response(['data' => $mockData]),
+            'api.giphy.com/v1/gifs/search*' => Http::response(['data' => $mockData]),
         ]);
 
         $service = new GiphyService();
         $response = $service->search('success');
+
+        $this->assertEquals($mockData, $response);
+        Http::assertSentCount(1);
+    }
+
+    public function test_get_request(): void
+    {
+        $mockId = 'abc123';
+        $mockData = [
+            'id' => $mockId
+        ];
+
+        Http::fake([
+            'api.giphy.com/v1/gifs/*' => Http::response(['data' => $mockData]),
+        ]);
+
+        $service = new GiphyService();
+        $response = $service->get($mockId);
 
         $this->assertEquals($mockData, $response);
         Http::assertSentCount(1);
